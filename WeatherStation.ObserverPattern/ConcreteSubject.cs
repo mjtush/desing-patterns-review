e9891be5ff22ddbd9c914a0e349eaec7cb921df4
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-13 14:00:38 +0100

    feat(pattern): Indroduce observer pattern diagram.

---
+++ WeatherStation.ObserverPattern/ConcreteSubject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherStation.ObserverPattern
+{
+    public class ConcreteSubject : ISubject
+    {
+        public int State
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        private void registerObserver()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void removeObserver()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void notifyObserver()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WeatherStation.ObserverPattern/ISubject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherStation.ObserverPattern
+{
+    public interface ISubject
+    {
+        IObserver IObserver { get; set; }
+
+        void registerObserver();
+        void removeObserver();
+        void notifyObserver();
+    }
+}
