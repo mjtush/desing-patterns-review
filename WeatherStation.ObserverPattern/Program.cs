2180c2904f15c8c7f6b9c73c237786f7d4105dd1
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-13 21:36:46 +0100

    refactor(progrem) Integrates forecast display to program.

--- WeatherStation.ObserverPattern/Program.cs
+++ WeatherStation.ObserverPattern/Program.cs
@@ -6,6 +6,7 @@
         {
             var weatherStationData = new WeatherStationData();
             _ = new CurrentDisplay(weatherStationData);
+            _ = new ForecastDisplay(weatherStationData);
 
             weatherStationData.SetMeasurements(32, pressure: 1000, hummidity: 80);
             weatherStationData.SetMeasurements(32, 89, 999);
