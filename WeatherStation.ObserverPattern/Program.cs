bc0701cd51598e5868919aa24eb745fc84321536
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-14 19:19:18 +0100

    feat(display): Intorduce statistics display.

--- WeatherStation.ObserverPattern/Program.cs
+++ WeatherStation.ObserverPattern/Program.cs
@@ -7,6 +7,7 @@
             var weatherStationData = new WeatherStationData();
             _ = new CurrentDisplay(weatherStationData);
             _ = new ForecastDisplay(weatherStationData);
+            _ = new StatisticsDisplay(weatherStationData);
 
             weatherStationData.SetMeasurements(32, pressure: 1000, hummidity: 80);
             weatherStationData.SetMeasurements(32, 89, 999);
--- WeatherStation.ObserverPattern/StatisticsDisplay.cs
+++ WeatherStation.ObserverPattern/StatisticsDisplay.cs
@@ -7,22 +7,53 @@ namespace WeatherStation.ObserverPattern
 {
     public class StatisticsDisplay : IObserver, IDisplayElement
     {
-        public WeatherStationData WeatherStationData
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        private float _currentTemp = 20f;
+        private readonly List<float> _historicalTemperatures = [];
+        private float _averageTemp;
+        private float _maxTemp;
+        private float _minTemp;
 
+
+        private readonly WeatherStationData _weatherStationData;
+
+        public StatisticsDisplay(WeatherStationData weatherStationData)
+        {
+            _weatherStationData = weatherStationData;
+            _weatherStationData.RegisterObserver(this);
+            _historicalTemperatures.Add(_currentTemp);
+        }
         public void Update()
         {
-            throw new System.NotImplementedException();
+            _currentTemp = _weatherStationData.Temperature;
+            _historicalTemperatures.Add(_currentTemp);
+
+            CalculateAverageTemp();
+            CalculateMinTemp();
+            CalculateMaxTemp();
+            
+            Display();
+        }
+
+        private void CalculateAverageTemp()
+        {
+            _averageTemp =  _historicalTemperatures.Average();
+        }
+
+        private void CalculateMinTemp()
+        {
+            _minTemp = _historicalTemperatures.Min();
+        }
+
+        private void CalculateMaxTemp()
+        {
+            _maxTemp = _historicalTemperatures.Max();
         }
 
         public void Display()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine($"Min/Avg/Max Temperature: {_minTemp}/{_averageTemp}/{_maxTemp} C.");
+            Console.WriteLine("------------------\n");
         }
+
     }
 }
