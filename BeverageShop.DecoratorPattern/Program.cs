1fc4cfecd6793cbc53ee0494f0012d88b32e92f2
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-20 19:11:14 +0100

    refactor(program): Integrate compnents and decorators to show interactions.

--- BeverageShop.DecoratorPattern/Program.cs
+++ BeverageShop.DecoratorPattern/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace BeverageShop.DecoratorPattern
+﻿using static System.Console;
+using BeverageShop.DecoratorPattern.Beverages;
+
+namespace BeverageShop.DecoratorPattern
 {
     internal class Program
     {
-        static void Main()
+        public static void Main()
         {
-            
+            Beverage beverage = new Espresso();
+
+            WriteLine($"{beverage.Description} ${beverage.Cost()}");
+
+            Beverage beverage2 = new DarkRoast();
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Whip(beverage2);
+
+            WriteLine($"{beverage2.Description} ${beverage2.Cost()}");
+
+            Beverage beverage3 = new DarkRoast();
+            beverage3 = new Soy(beverage3);
+            beverage3 = new Mocha(beverage3);
+            beverage3 = new Whip(beverage3);
+
+            WriteLine($"{beverage3.Description} ${beverage3.Cost()}");
         }
     }
 }
