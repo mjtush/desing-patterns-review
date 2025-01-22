484e3b375ace8efc480f812bb3d4406541d3cee9
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-22 22:03:57 +0100

    refactor(condiments): Udate Cost to handle different cup sizes. Introduce switch pattern matching.

--- BeverageShop.DecoratorPattern/Condiments/Whip.cs
+++ BeverageShop.DecoratorPattern/Condiments/Whip.cs
@@ -5,7 +5,9 @@ namespace BeverageShop.DecoratorPattern
 {
     internal class Whip : CondimentDecorator
     {
-        private readonly double _cost = 0.10;
+        private double _costForSmallSizeCup = 0.05;
+        private double _costForMiddleSizeCup = 0.10;
+        private double _costForBigSizeCup = 0.15;
 
         public Whip(Beverage beverage) : base(beverage)
         {
@@ -15,7 +17,16 @@ namespace BeverageShop.DecoratorPattern
 
         public override double Cost()
         {
-            return this.Beverage.Cost() + _cost;
+            var cost = Beverage.Cost();
+            cost += Beverage.Size switch
+            {
+                Size.Small => _costForSmallSizeCup,
+                Size.Medium => _costForMiddleSizeCup,
+                Size.Big => _costForBigSizeCup,
+                _ => 0
+            };
+
+            return cost;
         }
     }
 }
