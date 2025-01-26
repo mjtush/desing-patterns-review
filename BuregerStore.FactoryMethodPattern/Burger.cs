f7d6513be58ff423b1a5696191e23e707566b837
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-26 22:29:43 +0100

    feat(burgers) Add abstraction of a burger.

---
+++ BuregerStore.FactoryMethodPattern/Burger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BuregerStore.FactoryMethodPattern
+{
+    public abstract class Burger
+    {
+        public virtual void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Cook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Box()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
