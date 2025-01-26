73f178b477ec2e6abebc99544ed735ceaec08a16
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-26 22:29:14 +0100

    feat(stores): Add abstract class and concrete classes for store.

---
+++ BuregerStore.FactoryMethodPattern/BuregerStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuregerStore.FactoryMethodPattern
+{
+    abstract class BuregerStore
+    {
+        public Burger OrderBurger(string type)
+        {
+            Burger burger = CreateBurger(type);
+            burger.Prepare();
+            burger.Cook();
+            burger.Box();
+            return burger;
+        }
+
+        protected abstract Burger CreateBurger(string type);
+    }
+}
