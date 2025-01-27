08886ed89b3b271169b44aa5a68ee3e9c48c23c6
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2025-01-27 19:57:22 +0100

    feat: Implement concrete burger stores and burger types.

---
+++ BuregerStore.FactoryMethodPattern/PolishBurgerStore.cs
@@ -0,0 +1,56 @@
+﻿using BurgerStore.FactoryMethodPattern;
+
+namespace BurgerStore.FactoryMethodPattern
+{
+    internal class PolishBurgerStore : BurgerStore
+    {
+        protected override Burger CreateBurger(string type) => type switch
+        {
+            "beef" => new PolishBeefBurger(),
+            "chicken" => new PolishChickenBurger(),
+            "pork" => new PolishPorkBurger(),
+            "weggie" => new PolishWeggieBurger(),
+            _ => new PolishBeefBurger()
+        };
+    }
+
+    internal class PolishBeefBurger : Burger
+    {
+        public PolishBeefBurger()
+        {
+            Name = "Polish Beef Burger";
+            Roll = "White Roll";
+            Meat = "Grilled Beef";
+        }
+    }
+
+    internal class PolishChickenBurger : Burger
+    {
+        public PolishChickenBurger()
+        {
+            Name = "Polish Chicken Burger";
+            Roll = "White Roll";
+            Meat = "Grilled Chicken";
+        }
+    }
+
+    internal class PolishPorkBurger : Burger
+    {
+        public PolishPorkBurger()
+        {
+            Name = "Polish Pork Burger";
+            Roll = "White Roll";
+            Meat = "Pulled Pork";
+        }
+    }
+
+    internal class PolishWeggieBurger : Burger
+    {
+        public PolishWeggieBurger()
+        {
+            Name = "Polish Weggie Burger";
+            Roll = "Whole Wheat Roll";
+            Meat = "Veggie Patty";
+        }
+    }
+}
--- BuregerStore.FactoryMethodPattern/TexasBurgerStore.cs
+++ BuregerStore.FactoryMethodPattern/TexasBurgerStore.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace BurgerStore.FactoryMethodPattern
 {
-    internal class TexasBurgerStore
+    internal class TexasBurgerStore : BurgerStore
     {
+        protected override Burger CreateBurger(string type) => type switch
+        {
+            "beef" => new TexasBeefBurger(),
+            "chicken" => new TexasChickenBurger(),
+            "pork" => new TexasPorkBurger(),
+            "weggie" => new TexasWeggieBurger(),
+            _ => new TexasBeefBurger()
+        };
+    }
+
+    internal class TexasBeefBurger : Burger
+    {
+        public TexasBeefBurger()
+        {
+            Name = "Texas Beef Burger";
+            Roll = "White Roll";
+            Meat = "Grilled Beef";
+        }
+    }
+
+    internal class TexasChickenBurger : Burger
+    {
+        public TexasChickenBurger()
+        {
+            Name = "Texas Chicken Burger";
+            Roll = "White Roll";
+            Meat = "Grilled Chicken";
+        }
+    }
+
+    internal class TexasPorkBurger : Burger
+    {
+        public TexasPorkBurger()
+        {
+            Name = "Texas Pork Burger";
+            Roll = "White Roll";
+            Meat = "Pork Patty";
+        }
+    }
+
+    internal class TexasWeggieBurger : Burger
+    {
+        public TexasWeggieBurger()
+        {
+            Name = "Texas Weggie Burger";
+            Roll = "Whole Wheat Roll";
+            Meat = "Veggie Patty";
+        }
     }
 }
